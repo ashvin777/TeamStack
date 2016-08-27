bbdc35ea4422bbd8140753ca5c38698f639180c7
ashvin <ashvin.suthar777@gmail.com>
2016-08-28 00:40:33 +0530

    added router

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 (function(app) {
 
-  app.AppComponent =
-    ng.core.Component({
+  app.AppComponent = ng.core.Component({
       selector: "app",
-      directives: [ng.router.RouterLink]
+      directives: [ng.router.RouterOutlet, ng.router.RouterLink],
+      template: "<router-outlet></router-outlet>"
     })
     .Class({
-      constructor: function(router) {
-        console.log(router);
-      }
-    })
+      constructor: function() {}
+    });
 
-  app.AppModule =
-    ng.core.NgModule({
-      imports: [ng.platformBrowser.BrowserModule],
+  var routes = [{
+    path: '',
+    component: app.HomePage
+  }];
+
+  var routing = ng.router.RouterModule.forRoot(routes);
+
+  app.AppModule = ng.core.NgModule({
+      imports: [ng.platformBrowser.BrowserModule, routing],
       declarations: [app.AppComponent],
       bootstrap: [app.AppComponent]
     })
